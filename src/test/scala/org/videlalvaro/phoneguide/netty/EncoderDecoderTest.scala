507a8699e0e6e98c2426741d7d74de4b7f1abe8a
Alvaro Videla <videlalvaro@gmail.com>
2016-07-26 07:50:40 +0200

    adds EncoderDecoderTest

---
+++ src/test/scala/org/videlalvaro/phoneguide/netty/EncoderDecoderTest.scala
@@ -0,0 +1,23 @@
+package org.videlalvaro.phoneguide.netty
+
+import io.netty.channel.embedded.EmbeddedChannel
+import org.scalatest.prop.{Checkers, GeneratorDrivenPropertyChecks}
+import org.scalatest.{FunSuite, Matchers}
+import org.videlalvaro.phoneguide.PhoneNumber
+import org.videlalvaro.phoneguide.generators.Generators._
+
+class EncoderDecoderTest extends FunSuite with Checkers with GeneratorDrivenPropertyChecks with Matchers {
+  test("netty encode/decode message") {
+    forAll { (phoneNumber: PhoneNumber) =>
+      val channel = new EmbeddedChannel(new MessageEncoder(), new MessageDecoder())
+
+      channel.writeOutbound(phoneNumber)
+      channel.writeInbound(channel.readOutbound())
+
+      val readPhoneNumber = channel.readInbound();
+      readPhoneNumber should not be (null)
+      readPhoneNumber.equals(phoneNumber) should be (true)
+      phoneNumber.equals(readPhoneNumber) should be (true)
+    }
+  }
+}
